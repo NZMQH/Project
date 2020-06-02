64c6caa2186545bba2686c52faa31dd38e3e6e3e
NZMQH <yck_yt520@163.com>
2020-06-02 23:29:35 +0800

    后台布局页排版

---
+++ 毕业设计源码和数据库/毕业设计/201817380227易炽昆/Controllers/BackStageController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _201817380227易炽昆.Controllers
+{
+    public class BackStageController : Controller
+    {
+        // GET: BackStage
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
