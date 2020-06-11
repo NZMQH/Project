72f960096ef62ce49c58b358605e2365bc4eaf08
NZMQH <yck_yt520@163.com>
2020-06-11 23:46:05 +0800

    出租房的查询和添加

--- 毕业设计源码和数据库/毕业设计/201817380227易炽昆/Controllers/LeaseHouseController.cs
+++ 毕业设计源码和数据库/毕业设计/201817380227易炽昆/Controllers/LeaseHouseController.cs
@@ -11,9 +11,29 @@ namespace _201817380227易炽昆.Controllers
     {
         HomeDBEntities db = new HomeDBEntities();
         // GET: LeaseHouse
+        public ActionResult Index(string Position="")
+        {
+            var list = db.LeaseHouse.Where(p=>p.Position==Position || p.Position.Contains(Position)).ToList();
+            ViewBag.list = list;
+            return View();
+        }
+        [HttpPost]
         public ActionResult Index()
         {
+            var list = db.LeaseHouse.Where(p => p.IsLease == "否").ToList();
+            ViewBag.list = list;
             return View();
+        }
+        public ActionResult AddHouse()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddHouse(LeaseHouse house)
+        {
+            db.LeaseHouse.Add(house);
+            db.SaveChanges();
+            return Content("<script >alert('添加成功');window.open('" + Url.Content("/LeaseHouse/Index") + "', '_self')</script >", "text/html");
         }
     }
 }
