125ab34b4a62d903fca04c53e5e43cdc4b8d445e
NZMQH <yck_yt520@163.com>
2020-06-21 23:03:37 +0800

    UI主页面

--- 毕业设计源码和数据库/毕业设计/201817380227易炽昆/Controllers/UIController.cs
+++ 毕业设计源码和数据库/毕业设计/201817380227易炽昆/Controllers/UIController.cs
@@ -15,7 +15,24 @@ namespace _201817380227易炽昆.Controllers
         /// 前端首页
         /// </summary>
         /// <returns></returns>
-        public ActionResult Index()
+        public ActionResult Index(string UserLogin)
+        {
+            if (UserLogin==null)
+            {
+                Session["User"] = null;
+            }
+            else
+            {
+                var user= db.User.Where(p => p.UserLogin == UserLogin).FirstOrDefault();
+                Session["User"] = user;
+            }
+            var lease = db.LeaseHouse.Where(p => p.IsLease == "否").ToList();
+            var sell = db.SellHouse.Where(p => p.IsSell == "否").ToList();
+            ViewBag.lease = lease;
+            ViewBag.sell = sell;
+            return View();
+        }
+        public ActionResult SelectHouse()
         {
             return View();
         }
