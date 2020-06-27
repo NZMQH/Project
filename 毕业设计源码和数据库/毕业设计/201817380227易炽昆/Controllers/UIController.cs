cd9349c9e94bd0ec48bd8e09e264fa2c7d9c55ab
NZMQH <yck_yt520@163.com>
2020-06-27 23:07:23 +0800

    0

--- 毕业设计源码和数据库/毕业设计/201817380227易炽昆/Controllers/UIController.cs
+++ 毕业设计源码和数据库/毕业设计/201817380227易炽昆/Controllers/UIController.cs
@@ -17,13 +17,13 @@ namespace _201817380227易炽昆.Controllers
         /// <returns></returns>
         public ActionResult Index(string UserLogin)
         {
-            if (UserLogin==null)
+            if (UserLogin == null)
             {
                 Session["User"] = null;
             }
             else
             {
-                var user= db.User.Where(p => p.UserLogin == UserLogin).FirstOrDefault();
+                var user = db.User.Where(p => p.UserLogin == UserLogin).FirstOrDefault();
                 Session["User"] = user;
             }
             var lease = db.LeaseHouse.Where(p => p.IsLease == "否").ToList();
@@ -36,8 +36,43 @@ namespace _201817380227易炽昆.Controllers
         /// 租房
         /// </summary>
         /// <returns></returns>
-        public ActionResult LeaseHouse()
+        public ActionResult LeaseHouse(string Position = "")
         {
+            List<LeaseHouse> list = db.LeaseHouse.Where(p => (p.Position == Position && p.IsLease == "否") || (p.Position.Contains(Position) && p.IsLease == "否")).ToList();
+            ViewBag.list = list;
+            return View();
+        }
+        /// <summary>
+        /// 租房详情页
+        /// </summary>
+        /// <param name="LeaseID"></param>
+        /// <returns></returns>
+        public ActionResult LeaseHouseMsg(int LeaseID)
+        {
+            LeaseHouse leaseHouse = db.LeaseHouse.Find(LeaseID);
+            ViewBag.leaseHouse = leaseHouse;
+            return View();
+        }
+        /// <summary>
+        /// 买房
+        /// </summary>
+        /// <param name="Position"></param>
+        /// <returns></returns>
+        public ActionResult SellHouse(string Position="")
+        {
+            List<SellHouse> list = db.SellHouse.Where(p => (p.Position == Position && p.IsSell=="否") || (p.Position.Contains(Position) && p.IsSell=="否")).ToList();
+            ViewBag.list = list;
+            return View();
+        }
+        /// <summary>
+        /// 买房详情页
+        /// </summary>
+        /// <param name="SellID"></param>
+        /// <returns></returns>
+        public ActionResult SellHouseMsg(int SellID)
+        {
+            SellHouse sellHouse = db.SellHouse.Find(SellID);
+            ViewBag.sellHouse = sellHouse;
             return View();
         }
     }
