cd034a5d398cc4ffb299030d92827ae8a5846c22
NZMQH <yck_yt520@163.com>
2020-07-01 22:14:12 +0800

    前端

--- 毕业设计源码和数据库/毕业设计/201817380227易炽昆/Controllers/UI1Controller.cs
+++ 毕业设计源码和数据库/毕业设计/201817380227易炽昆/Controllers/UI1Controller.cs
@@ -11,6 +11,11 @@ namespace _201817380227易炽昆.Controllers
     {
         HomeDBEntities db = new HomeDBEntities();
         // GET: UI1
+        /// <summary>
+        /// 首页
+        /// </summary>
+        /// <param name="UserLogin"></param>
+        /// <returns></returns>
         public ActionResult Index(string UserLogin)
         {
             if (UserLogin == null)
@@ -28,5 +33,59 @@ namespace _201817380227易炽昆.Controllers
             ViewBag.sell = sell;
             return View();
         }
+        /// <summary>
+        /// 租房页
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Lease( string Position = "")
+        {
+            ViewBag.leaseHouse= db.LeaseHouse.Where(p => (p.Position == Position && p.IsLease == "否") || (p.Position.Contains(Position) && p.IsLease == "否")).ToList();
+            return View();
+        }
+        /// <summary>
+        /// 租房详情
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult LeaseHouseMsg(int LeaseID)
+        {
+            var leaseHouse = db.LeaseHouse.Find(LeaseID);
+            var pic = db.Picture.Where(p => p.LeaseID == LeaseID).ToList();
+            ViewBag.leaseHouse = leaseHouse;
+            ViewBag.pic = pic;
+            return View();
+        }
+
+        /// <summary>
+        /// 购房页
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Sell(string Position = "")
+        {
+            ViewBag.sellHouse = db.SellHouse.Where(p => (p.Position == Position && p.IsSell == "否") || (p.Position.Contains(Position) && p.IsSell == "否")).ToList();
+            return View();
+        }
+        /// <summary>
+        /// 购房详情
+        /// </summary>
+        /// <param name="SellID"></param>
+        /// <returns></returns>
+        public ActionResult SellHouseMsg(int SellID)
+        {
+            var sellHouse = db.SellHouse.Find(SellID);
+            var pic = db.Picture.Where(p => p.SellID == SellID).ToList();
+            ViewBag.sellHouse = sellHouse;
+            ViewBag.pic = pic;
+            return View();
+        }
+
+        /// <summary>
+        /// 关于
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult About()
+        {
+            return View();
+        }
+
     }
 }
